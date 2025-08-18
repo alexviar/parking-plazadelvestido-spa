df05cf58fc2f9fa0cd97adaf91b3ee52a7472cd3
Alexander <alexander.va@hotmail.com>
2025-08-18 16:32:43 -0600

    feat: add PrivateRoute component for authentication flow
    Implement PrivateRoute to handle authenticated routes and redirect unauthenticated users to login page. Also handles redirecting authenticated users away from login page if they're already logged in.

---
+++ src/features/auth/components/PrivateRoute.tsx
@@ -0,0 +1,19 @@
+import { useSelector } from "react-redux"
+import { Navigate, Outlet, useLocation, useMatch } from "react-router"
+import { type AuthState } from "../redux/authSlice"
+
+export const PrivateRoute = () => {
+    const matchLogin = useMatch('login')
+    const location = useLocation()
+    const { from = '/' } = location.state ?? {}
+    const user = useSelector((state: { auth: AuthState }) => state.auth.user)
+
+    if (!user && !matchLogin) {
+        return <Navigate to="login" state={{ from: location.pathname }} replace />
+    }
+    if (user && matchLogin) {
+        return <Navigate to={from} replace />
+    }
+
+    return <Outlet />
+}
--- src/setup/router.tsx
+++ src/setup/router.tsx
@@ -1,9 +1,15 @@
 import { createBrowserRouter } from "react-router";
+import { PrivateRoute } from "../features/auth/components/PrivateRoute";
 import { TicketScanner } from "../features/tickets/components/TicketScanner";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <TicketScanner />
+    element: <PrivateRoute />,
+    children: [
+      {
+        path: '/',
+        element: <TicketScanner />
+      }
+    ]
   }
 ]);
