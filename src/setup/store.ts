22dc28896c123df424aece64b283b42ad6d82e9b
Alexander <alexander.va@hotmail.com>
2025-08-18 16:30:42 -0600

    squash with base api

--- src/setup/store.ts
+++ src/setup/store.ts
@@ -1,9 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
 import { persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
+import { appApi } from '../commons/api/appApi'
 import { offlineQueueSlice } from '../features/tickets/redux/offlineQueueSlice'
 
 const rootReducer = combineReducers({
+  [appApi.reducerPath]: appApi.reducer,
   offlineQueue: offlineQueueSlice.reducer,
 })
 
@@ -21,7 +24,11 @@ export const store = configureStore({
       serializableCheck: {
         ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE', 'persist/PAUSE', 'persist/PURGE', 'persist/REGISTER', 'persist/FLUSH'],
       },
-    }),
+    }).concat(
+      appApi.middleware
+    )
 })
+
+setupListeners(store.dispatch)
 
 export const persistor = persistStore(store)
