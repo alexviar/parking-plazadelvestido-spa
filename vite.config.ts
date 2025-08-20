6e0010752b28cfe10220c00c4beb6fca7aa08177
Alexander <alexander.va@hotmail.com>
2025-08-19 23:08:18 -0600

    feat: add basic configuration for progressive web app with  vite-plugin-pwa
    Add vite-plugin-pwa dependency and configure it in vite.config.ts to enable PWA capabilities

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,15 @@ import tailwindcss from '@tailwindcss/vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    basicSsl()
+    basicSsl(),
+    VitePWA()
   ],
   server: {
     host: true
